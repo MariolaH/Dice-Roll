e9cd04dbfd6f4ff84da0b575551e03fbe691b174
Mariola <mariolahullings@gmail.com>
2023-09-10 14:54:51 -0400

    Added functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //  WHO         WHAT    VALUE
+//        diceImageView1.image = UIImage(named: "DiceSix")
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        // .randomElement() and Int.random do the samething
+        diceImageView1.image = UIImage(named: diceArray.randomElement() ?? "DiceOne")
+        diceImageView2.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+    }
 }
-
